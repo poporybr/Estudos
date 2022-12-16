3b9048985abbbd800bb09ec410c27a9d2a85fb15
Eduardo Marques <112179103+poporybr@users.noreply.github.com>
2022-12-16 13:18:04 -0300

    Create exEscalandoTime.js

---
+++ exEscalandoTime.js
@@ -0,0 +1,19 @@
+function addPlayer() {
+  const position = document.getElementById("position").value
+  const name = document.getElementById("name").value
+  const number = document.getElementById("number").value
+
+  const confirmation = cfirm("Escalar " + name + " como " + position + "?")
+
+  if (confirmation) {
+    const teamList = document.getElementById("team-list")
+    const playerItem = document.createElement("li")
+    playerItem.id = "player-" + number
+    playerItem.innerText = position + ": " + name + " (" + number + ")"
+    teamList.appendChild(playerItem)
+
+    document.getElementById("position").value = ""
+    document.getElementById("name").value = ""
+    document.getElementById("number").value = ""
+  }
+}
