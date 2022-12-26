c4cacdbf772e1294aa4fba1f3ea8d2db35d6f1a5
Eduardo Marques <112179103+poporybr@users.noreply.github.com>
2022-12-26 16:38:48 -0300

    Create ESModules.js

---
+++ ESModules.js
@@ -0,0 +1,26 @@
+// functions.js
+
+export function label(attributes) {
+  const element = document.createElement('label')
+  Object.assign(element, attributes)
+  return element
+}
+
+export function input(attributes) {
+  const element = document.createElement('input')
+  Object.assign(element, attributes)
+  return element
+}
+
+export function br() {
+  const element = document.createElement('br')
+  return element
+}
+
+// index.js
+
+import { label, input, br } from './functions.js'
+
+console.log(label({ for: 'fullname', textContent: 'Nome Completo'}))
+console.log(input({ id: 'fullname', name: 'fullname', placeholder: 'Digite seu nome completo...'}))
+console.log(br())
