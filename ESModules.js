e7598004ff0f7c75bd174d7430b9f31fbbcab562
Eduardo Marques <112179103+poporybr@users.noreply.github.com>
2023-01-14 15:27:03 -0300

    Update ESModules.js

--- ESModules.js
+++ ESModules.js
@@ -24,3 +24,4 @@ import { label, input, br } from './functions.js'
 console.log(label({ for: 'fullname', textContent: 'Nome Completo'}))
 console.log(input({ id: 'fullname', name: 'fullname', placeholder: 'Digite seu nome completo...'}))
 console.log(br())
+//
