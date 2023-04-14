c955abffef44bc1edea8bb2de0e559fd466d92a0
Eduardo Marques <112179103+poporybr@users.noreply.github.com>
2023-04-14 17:54:29 -0300

    Add files via upload

---
+++ projetos/calculadora/index.js
@@ -0,0 +1,7 @@
+let entrada = document.getElementById('inputN').value
+let saida = document.getElementById('nResult').value  
+
+const somar = document.getElementById('btnMais')
+somar.addEventListener("click", function adicao(){
+    
+})
