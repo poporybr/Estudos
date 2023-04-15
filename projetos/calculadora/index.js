4a1533923bac0aed0d30b1da199375b71102d79e
Eduardo Marques <112179103+poporybr@users.noreply.github.com>
2023-04-15 12:39:05 -0300

    update

--- projetos/calculadora/index.js
+++ projetos/calculadora/index.js
@@ -1,7 +1,41 @@
-let entrada = document.getElementById('inputN').value
-let saida = document.getElementById('nResult').value  
+const somar = document.querySelector('#btnSomar');
+somar.addEventListener("click", () => {
+    let entrada = parseFloat(document.getElementById('inputN').value)
+    let parag =  document.getElementById('nResult')
+    let saida = parseFloat(parag.innerText)
+    document.getElementById('nResult').innerHTML = entrada + saida
+    document.getElementById('inputN').value = 0
+})
 
-const somar = document.getElementById('btnMais')
-somar.addEventListener("click", function adicao(){
-    
+const subtrair = document.querySelector('#btnMenos');
+subtrair.addEventListener("click", () => {
+    let entrada = parseFloat(document.getElementById('inputN').value)
+    let parag =  document.getElementById('nResult')
+    let saida = parseFloat(parag.innerText)
+    document.getElementById('nResult').innerHTML = saida - entrada
+    document.getElementById('inputN').value = 0
+})
+
+const multiplicar = document.querySelector('#btnVezes');
+multiplicar.addEventListener("click", () => {
+    let entrada = parseFloat(document.getElementById('inputN').value)
+    let parag =  document.getElementById('nResult')
+    let saida = parseFloat(parag.innerText)
+    document.getElementById('nResult').innerHTML = entrada * saida
+    document.getElementById('inputN').value = 0
+})
+
+const dividir = document.querySelector('#btnDividido');
+dividir.addEventListener("click", () => {
+    let entrada = parseFloat(document.getElementById('inputN').value)
+    let parag =  document.getElementById('nResult')
+    let saida = parseFloat(parag.innerText)
+    document.getElementById('nResult').innerHTML = saida / entrada
+    document.getElementById('inputN').value = 0
+})
+
+const limpar = document.getElementById('btnClear');
+limpar.addEventListener("click", () => {
+    parseFloat(document.getElementById('inputN').value = 0)
+    document.getElementById('nResult').innerHTML = 0
 })
